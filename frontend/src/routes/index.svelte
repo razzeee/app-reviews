258e377941a57039c87c041686a0c80c5dda679e
Razze <razzeee@gmail.com>
2021-08-11 21:24:22 +0200

    Initialize frontend

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<button class="btn btn-primary">DaisyUI Button</button>
+</section>
+
+<style>
+</style>
