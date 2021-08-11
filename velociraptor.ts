3b33987a49a848a17c456260b6072a3eed48be50
Razze <razzeee@gmail.com>
2021-08-11 18:26:20 +0200

    Move export to it's own folder

--- velociraptor.ts
+++ velociraptor.ts
@@ -1,7 +1,7 @@
 export default {
   scripts: {
     export: {
-      cmd: "deno run --allow-net --allow-write mod.ts",
+      cmd: "deno run --allow-net --allow-write export/mod.ts",
       desc: "Runs the scraper to create the apps.json",
     },
     lint: "deno lint",
